916c457f3159e734fca3c412f267ab062b90452c
migaria <miarias1417@gmail.com>
2026-03-22 20:23:49 -0500

    Backend Iniciado

---
+++ src/main/java/com/agromarket/agromarket_backend/AgromarketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agromarket.agromarket_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgromarketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgromarketBackendApplication.class, args);
+	}
+
+}
